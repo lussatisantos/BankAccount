41434562d2a9f9c64686c82a41ea848613adacf4
Lussati Santos <lussatisantos10@gmail.com>
2024-08-21 23:01:00 +0100

    Initial commit

---
+++ src/bankAccount.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class bankAccount {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.UK);
+
+        int agencyNumber;
+        int accountNumber;
+        String clientName;
+        double balance;
+
+        System.out.print("Enter agency number: ");
+        agencyNumber = scanner.nextInt();
+        System.out.print("Enter account number: ");
+        accountNumber = scanner.nextInt();
+        System.out.print("Enter client name: ");
+        clientName = scanner.next();
+        System.out.print("Enter deposit amount: ");
+        balance = scanner.nextDouble();
+
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Agency Number: " + agencyNumber);
+        System.out.println("Client Name: " + clientName);
+        System.out.println("Current Balance: " + balance);
+
+        scanner.close();
+    }
+}
